aa00c20e9dbee3ea5969271252878aa5e611b1ff
Mohamed <123790679+sushimunchi@users.noreply.github.com>
2024-04-16 07:41:07 +0800

    Revert "Updated login and register with php functionalities and style change"
    This reverts commit da2c01bfbc7c279888a48210455f6ef127c94392.

--- Login_Register_UI/login.php
+++ Login_Register_UI/login.php
@@ -1,57 +1,45 @@
-<?php include('Php/functions.php'); ?>
 <!DOCTYPE html>
 <html data-bs-theme="light" lang="en">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>FURCO</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="Style/styles.min.css">
 </head>
-<body style="background: url('Image/pawsfull.svg') left / contain no-repeat, rgb(234,192,162);">
-<nav class="navbar navbar-expand py-3" style="background: rgb(255,197,158);">
-    <div class="container-fluid">
-        <a class="navbar-brand d-flex align-items-center" href="#">
-            <img src="Image/logo.svg">
-            <span style="font-family: Ubuntu, sans-serif; font-weight: bold; font-size: 24px; color: rgb(0,0,0);">FURCO</span>
-        </a>
-        <input class="form-control-lg" type="search" name="Search" placeholder="Search" style="border-radius: 22px; border: 2px solid rgb(0,0,0); font-family: Poppins, sans-serif; margin-right: auto;">
-        <div class="d-flex align-items-center">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="Home.php" style="font-size: 24px; font-family: Poppins, sans-serif; color: rgb(0,0,0);">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="Product.php" style="font-size: 24px; font-family: Poppins, sans-serif; color: rgb(0,0,0);">Products</a></li>
-            </ul>
-            <a class="btn btn-primary" role="button" href="register.php" style="background: #ff8e3c; font-family: Roboto, sans-serif; width: 114px; height: 43px; border-color: #ff8e3c; border-radius: 22px; box-shadow: 0 5px 10px rgb(0,0,0); font-weight:bold;">Sign Up</a>
+
+<body style="background: url(&quot;assets/img/pawsfull.svg&quot;) left / contain no-repeat, rgb(234,192,162);">
+    <nav class="navbar navbar-expand-md py-3" style="background: rgb(255,197,158);border-right: 0px hidden rgba(255,255,255,0) ;">
+        <div class="container-fluid"><a class="navbar-brand d-flex align-items-center"><img src="assets/img/logo.svg"><span style="font-family: Ubuntu, sans-serif;font-weight: bold;font-size: 24px;">FURCO</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse justify-content-center" id="navcol-2">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item d-flex justify-content-center align-items-center"><input class="form-control-lg" type="search" name="Search" value="Search" style="border-radius: 22px;border: 2px solid rgb(0,0,0);font-family: Poppins, sans-serif;"></li>
+                    <li class="nav-item"><a class="nav-link active" href="#" style="font-size: 24px;font-family: Poppins, sans-serif;">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" style="font-family: Poppins, sans-serif;font-size: 24px;color: rgb(0,0,0);">Products</a></li>
+                </ul><a class="btn btn-primary d-flex justify-content-center align-items-center ms-md-2" role="button" href="register.html" style="background: #ff8e3c;font-family: Roboto, sans-serif;width: 114px;height: 43px;border-color: rgba(255,255,255,0);border-radius: 22px;box-shadow: 0px 5px 10px 0px rgb(0,0,0);">Sign Up</a>
+            </div>
         </div>
-    </div>
-</nav>
-    <section class="d-flex justify-content-center align-items-center py-4 py-xl-5" style="min-height: 100vh;">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card" style="background: #f9c180; border-radius: 19px; box-shadow: 0 5px 10px;">
-                    <div class="card-body">
-                        <h2 style="font-family: Poppins, sans-serif; border-bottom: 2px solid; padding-bottom: 10px;">Login</h2>
-                        <p>Welcome Back! Sign in to your account.</p>
-                        <form method="post" action="login.php">
-                            <?php echo display_error(); ?>
-                            <div class="mb-3">
-                                <label class="form-control-plaintext" style="font-weight: bold; font-family: Poppins, sans-serif;">Email Address *</label>
-                                <input class="form-control" type="email" name="email" style="border-radius: 22px; font-family: Poppins, sans-serif;" required>
-                            </div>
-                            <div class="mb-3">
-                                <label class="form-control-plaintext" style="font-weight: bold; font-family: Poppins, sans-serif;">Password *</label>
-                                <input class="form-control" type="password" name="password" style="border-radius: 22px; font-family: Poppins, sans-serif;" required>
-                            </div>
-                            <button class="btn btn-primary w-100" type="submit" style="background: #ff8e3c; font-family: Poppins, sans-serif; border-color: #ff8e3c; border-radius: 22px; color: black; font-weight: bold;">Login</button>
-                            <p class="text-muted">Don't have an account? <a href="register.php"><strong>Sign Up</strong></a></p>
-                        </form>
+    </nav>
+    <section class="align-items-center position-relative py-4 py-xl-5">
+        <div class="container-fluid d-grid justify-content-center align-items-center">
+            <div class="card d-grid mb-5" style="width: 637px;height: 417px;border-radius: 19px;background: #ffffff00;">
+                <div class="card-body" style="background: #f9c180;width: 637px;height: 417px;border-radius: 19px;box-shadow: 0px 5px 10px;">
+                    <div style="width: 592.2969px;border-bottom-width: 2px;border-bottom-style: solid;">
+                        <h2 style="font-family: Poppins, sans-serif;">Login</h2>
                     </div>
+                    <p class="w-lg-50" style="font-family: Poppins, sans-serif;">Welcome Back! Sign in to your account.</p>
+                    <form class="text-center">
+                        <div class="mb-3"><input class="form-control-plaintext" type="text" value="Email Address *" readonly="" style="font-weight: bold;font-family: Poppins, sans-serif;font-size: 14px;"><input class="form-control" type="email" name="email" style="border-radius: 22px;font-family: Poppins, sans-serif;" required=""></div>
+                        <div class="mb-3"><input class="form-control-plaintext" type="text" value="Password *" readonly="" style="font-size: 14px;font-family: Poppins, sans-serif;font-weight: bold;"><input class="form-control" type="password" name="password" style="border-radius: 22px;font-family: Poppins, sans-serif;" required=""></div>
+                        <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit" style="background: #ff8e3c;font-family: Poppins, sans-serif;font-size: 14px;border-radius: 22px;border-color: #ffffff00;color: rgb(0,0,0);font-weight: bold;">Login</button></div>
+                        <p class="text-muted" style="font-family: Poppins, sans-serif;font-size: 14px;">Don't have an account? <a href="register.html"><strong>Sign Up</strong></a></p>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
     <footer class="d-flex justify-content-center align-items-center" style="background: #9e6b2f;padding: 25px;">
         <div class="container">
             <div class="row">
@@ -73,9 +61,9 @@
                     <h3 class="text-uppercase" style="color: rgb(255,255,255);">OPEN HOURS</h3>
                     <p style="color: rgb(255,255,255);">8am - 8pm<br> Monday-Saturday<br><br> For inquiries during holidays, please contact us! </p>
                 </div>
-                <div class="col-md-3">
-                <h3 class="text-uppercase" style="color: rgb(255,255,255);">QUICK LINKS</h3>
-                    <div class="d-grid footer-links"><a href="#" style="color: rgb(255,255,255);">Home</a><br><a href="#" style="color: rgb(255,255,255);">Subscriptions</a><br><a href="#" style="color: rgb(255,255,255);">Products</a><br><a href="#" style="color: rgb(255,255,255);">My Orders</a></div>
+                <div class="col-md-3 d-flex justify-content-start align-items-center">
+                    <h3 class="text-uppercase"></h3><br>
+                    <div class="d-grid footer-links"><a href="#" style="color: rgb(255,255,255);">Home</a><a href="#" style="color: rgb(255,255,255);">Subscriptions</a><a href="#" style="color: rgb(255,255,255);">Products</a><a href="#" style="color: rgb(255,255,255);">My Orders</a></div>
                 </div>
             </div>
             <hr>
@@ -89,4 +77,5 @@
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
